274ceb0943f759dc363dd6e7ea0ee25b2f13aa38
NatalyaDashkovska <dashkovsk@gmail.com>
2021-01-13 02:42:24 +0200

    3 +-

---
+++ js/task-2.js
@@ -0,0 +1,20 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+
+
+
+
+// const addList = ingredients.forEach(ingredients => 
+// {const elementList = document.createElement(`li`);
+//     elementList.textContent = ingredients;
+//     const listInput = document.querySelector(`#ingredients`);
+// listInput.append(elementList);
+// return elementList }
+// );
---
+++ js/task-3.js
@@ -0,0 +1,44 @@
+// Напиши скрипт для создания галлереи изображений по массиву данных.
+
+// В HTML есть список ul#gallery.
+
+// <ul id="gallery"></ul>
+// Используй массив объектов images для создания тегов img вложенных в li. 
+// Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
+const images = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+
+const addList = images.forEach(images => 
+{
+    const elementList = document.createElement(`li`);
+     const aaa = document.createElement(`img`)
+aaa.classList.add(`photo`);
+ aaa.setAttribute('src', images.url);
+    aaa.setAttribute('alt', images.alt);
+    elementList.appendChild(aaa);
+
+    // elementList.insertAdjacentHTML('afterbegin', aaa)
+    console.log(aaa);
+    const listInput = document.querySelector(`#gallery`);
+listInput.append(elementList);
+return elementList }
+);
